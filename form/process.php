7024faa7e7bb3217b11e3b04ac6a860e1665a67a
akashbarot <akashbarot985@gmail.com>
2014-07-25 16:54:38 +0530

    email template 4

--- form/process.php
+++ form/process.php
@@ -181,7 +181,7 @@ function updateProcess($did){
 				$message .= '<tr><td>&nbsp;</td><td><p style="font:22px Helvetica Neue,Helvetica,Arial,sans-serif;font-weight:bold;margin:0px 0 0 0;padding:0;color:#000;">Congratulations</p><p style="font:14px Helvetica Neue,Helvetica,Arial,sans-serif;color:#777777;margin:20px 0 5px 0;padding:0;">Thanks for signing up for your free local guide from TownWizard! Click the email verification link below to complete the guide setup process.</p></td><td>&nbsp;</td></tr>';
 				$message .= '<tr><td height="100">&nbsp;</td><td> 
 									<p style="font:14px Helvetica Neue,Helvetica,Arial,sans-serif;color:#777777;margin:20px 0 5px 0;padding:0;">Link:</p>
-									<a style="font:14px Helvetica Neue,Helvetica,Arial,sans-serif;color:#1a1a1a;text-decoration:none;" target="_blank" href='.$link.' >'.$link.'</a>
+									<a style="font:12px Helvetica Neue,Helvetica,Arial,sans-serif;color:#1a1a1a;text-decoration:none;" target="_blank" href='.$link.' >'.$link.'</a>
 								</td><td>&nbsp;</td></tr>';
 				$message .= '<tr><td height="100">&nbsp;</td><td> 
 									<p style="font:14px Helvetica Neue,Helvetica,Arial,sans-serif;color:#777777;margin:20px 0 5px 0;padding:0;">Sincerely,</p>
